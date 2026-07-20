fac13bb6b34ccf133e356058795a094a950443ec
Surya Prakash <suryaprakash2008as@gmail.com>
2026-07-20 14:48:26 +0530

    Implement minOperations method to calculate operations

---
+++ 1769.minimun number of operation.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int[] answer = new int[n];
+        
+        // First Pass: Left to Right
+        int balls = 0;
+        int operations = 0;
+        for (int i = 0; i < n; i++) {
+            answer[i] += operations;
+            if (boxes.charAt(i) == '1') {
+                balls++;
+            }
+            operations += balls;
+        }
+        
+        // Second Pass: Right to Left
+        balls = 0;
+        operations = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] += operations;
+            if (boxes.charAt(i) == '1') {
+                balls++;
+            }
+            operations += balls;
+        }
+        
+        return answer;
+    }
+}
