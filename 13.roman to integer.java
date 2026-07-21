94e4bdea75e96e1767683d90b57d418d4608e6f7
Surya Prakash <suryaprakash2008as@gmail.com>
2026-07-21 10:55:37 +0530

    Create 13.roman to integer.java

---
+++ 13.roman to integer.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        int prevValue = 0;
+
+        // Traverse the string from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = getValue(s.charAt(i));
+
+            // If the current value is less than the previous value, subtract it
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                // Otherwise, add it
+                total += currentValue;
+            }
+
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+
+    private int getValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0;
+        }
+    }
+}
