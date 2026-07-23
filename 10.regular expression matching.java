39cfc63a2a71238cff9ef0b0a9cca887cd281db5
Surya Prakash <suryaprakash2008as@gmail.com>
2026-07-23 08:38:17 +0530

    Implement regular expression matching with dynamic programming

---
+++ 10.regular expression matching.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length();
+        int n = p.length();
+        
+        // dp[i][j] will be true if s[0..i-1] matches p[0..j-1]
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        
+        // Empty string matches empty pattern
+        dp[0][0] = true;
+        
+        // Handle patterns like a*, a*b*, or a*b*c* matching an empty string
+        for (int j = 2; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 2];
+            }
+        }
+        
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                char sc = s.charAt(i - 1);
+                char pc = p.charAt(j - 1);
+                
+                if (pc == sc || pc == '.') {
+                    // Current characters match
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (pc == '*') {
+                    // Case 1: Treat '*' as zero occurrences of the preceding element
+                    dp[i][j] = dp[i][j - 2];
+                    
+                    // Case 2: Treat '*' as one or more occurrences if the preceding element matches
+                    char prevChar = p.charAt(j - 2);
+                    if (prevChar == sc || prevChar == '.') {
+                        dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
