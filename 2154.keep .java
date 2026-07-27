9e822161811f59a4304fc420be8f7c43927acd31
Surya Prakash <suryaprakash2008as@gmail.com>
2026-07-27 10:48:19 +0530

    Create 2154.keep .java

---
+++ 2154.keep .java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        
+        while (set.contains(original)) {
+            original *= 2;
+        }
+        
+        return original;
+    }
+}
